43c78fc84edee4e1f097af044e1b7f51cb0559ad
annetouane <58571692+annetouane@users.noreply.github.com>
2021-01-24 13:59:49 +0100

    "e"

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -11,7 +11,7 @@ const api =
         },
       })
     : axios.create({
-        baseURL: "http://localhost:5000/",
+        baseURL: "http://localhost:5000/api/",
         headers: {
           "Content-Type": "application/json",
         },
